4c51d3d978337ef45dcd20b2491e674bd8741184
jmagaram <justin@magaram.com>
2024-11-11 14:15:59 -0800

    add more basic gentype tests

--- src/Simple.gen.tsx
+++ src/Simple.gen.tsx
@@ -8,3 +8,7 @@ import * as SimpleJS from './Simple.res.mjs';
 import type {t as Core__JSON_t} from '@rescript/core/src/Core__JSON.gen';
 
 export const hello: Core__JSON_t = SimpleJS.hello as any;
+
+export const add: (a:number, b:number) => number = SimpleJS.add as any;
+
+export const isNone: <T1>(v:(undefined | T1)) => boolean = SimpleJS.isNone as any;
--- src/Simple.res
+++ src/Simple.res
@@ -1,2 +1,8 @@
 @genType
 let hello = JSON.Encode.string("hello")
+
+@genType
+let add = (a, b) => a + b
+
+@genType
+let isNone = v => v->Option.isNone
